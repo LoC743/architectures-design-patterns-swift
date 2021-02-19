262956ef9b7b9a9f546fd777cfcc5aa747b31e83
Alexey Morozov <alexey1562@gmail.com>
2021-02-19 13:36:12 +0300

    [Урок 1]: Инициализация проекта

---
+++ WhoWantsToBeAmillionaire/WhoWantsToBeAmillionaire/Controller/MenuViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MenuViewController.swift
+//  WhoWantsToBeAmillionaire
+//
+//  Created by Alexey on 19.02.2021.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
