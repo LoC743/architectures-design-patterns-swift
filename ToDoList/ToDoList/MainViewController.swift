549e3d2e58e475cdcfa4c1aa53b44d3d48075aa0
Alexey Morozov <alexey1562@gmail.com>
2021-03-04 13:40:22 +0300

    [Урок 5]: Добавлен проект ToDoList

---
+++ ToDoList/ToDoList/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  ToDoList
+//
+//  Created by Alexey on 04.03.2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
