e51595870efb25c722d5bff289ee949efbb7d884
Alexey Morozov <alexey1562@gmail.com>
2021-03-05 14:31:12 +0300

    [Урок 5]: Реализован паттерн Chain of Responsibility. Добавлен вывод данных.

--- Lesson5_HW_ChainOfResponsibility.playground/Contents.swift
+++ Lesson5_HW_ChainOfResponsibility.playground/Contents.swift
@@ -1,6 +1,8 @@
 import UIKit
 import Foundation
 
+// MARK: Data
+
 func data(from file: String) -> Data {
     let path1 = Bundle.main.path(forResource: file, ofType: "json")!
     let url = URL(fileURLWithPath: path1)
@@ -13,7 +15,7 @@ let data2 = data(from: "2")
 let data3 = data(from: "3")
 
 
-// MARK: Soulution
+// MARK: Person
 
 struct Person: CustomStringConvertible {
     var name: String
@@ -24,6 +26,8 @@ struct Person: CustomStringConvertible {
         return "\nstruct Person:\nname: \(name)\nage: \(age)\nisDeveloper: \(isDeveloper)\n"
     }
 }
+
+// MARK: JSON Parsers
 
 class FirstJSONPaser: Decodable {
     var persons: [Person] = []
@@ -58,11 +62,6 @@ class FirstJSONPaser: Decodable {
     }
 }
 
-//
-//if let requestData = try? JSONDecoder().decode(FirstJSONPaser.self, from: data1) {
-//    print(requestData.persons)
-//}
-
 class SecondJSONPaser: Decodable {
     var persons: [Person] = []
     
@@ -96,11 +95,6 @@ class SecondJSONPaser: Decodable {
     }
 }
 
-//
-//if let requestData = try? JSONDecoder().decode(SecondJSONPaser.self, from: data2) {
-//    print(requestData.persons)
-//}
-
 class ThirdJSONPaser: Decodable {
     var persons: [Person] = []
     
@@ -128,6 +122,73 @@ class ThirdJSONPaser: Decodable {
     }
 }
 
-//if let requestData = try? JSONDecoder().decode(ThirdJSONPaser.self, from: data3) {
-//    print(requestData.persons)
-//}
+// MARK: - Chain of responsibility
+
+protocol Handler {
+    var nextHandler: Handler? { get set }
+    func handle(data: Data) -> [Person]?
+}
+
+class FirstJSONParserHandler: Handler {
+    var nextHandler: Handler?
+    
+    func handle(data: Data) -> [Person]? {
+        if let requestData = try? JSONDecoder().decode(FirstJSONPaser.self, from: data) {
+            return requestData.persons
+        } else {
+            return nextHandler?.handle(data: data)
+        }
+    }
+}
+
+class SecondJSONParserHandler: Handler {
+    var nextHandler: Handler?
+    
+    func handle(data: Data) -> [Person]? {
+        if let requestData = try? JSONDecoder().decode(SecondJSONPaser.self, from: data) {
+            return requestData.persons
+        } else {
+            return nextHandler?.handle(data: data)
+        }
+    }
+}
+
+class ThirdJSONParserHandler: Handler {
+    var nextHandler: Handler?
+    
+    func handle(data: Data) -> [Person]? {
+        if let requestData = try? JSONDecoder().decode(ThirdJSONPaser.self, from: data) {
+            return requestData.persons
+        } else {
+            return nextHandler?.handle(data: data)
+        }
+    }
+}
+
+// MARK: - Instantiate JSON pasers handlers
+
+let firstJSONParserHandler = FirstJSONParserHandler()
+let secondJSONParserHandler = SecondJSONParserHandler()
+let thirdJSONParserHandler = ThirdJSONParserHandler()
+
+firstJSONParserHandler.nextHandler = secondJSONParserHandler
+secondJSONParserHandler.nextHandler = thirdJSONParserHandler
+thirdJSONParserHandler.nextHandler = nil
+
+func printPersonsOptional(_ persons: [Person]?) {
+    if let persons = persons {
+        print(persons)
+    } else {
+        print("[Error!]: Failed to parse JSON for Person")
+    }
+}
+
+print("____________________________________ DATA 1 ____________________________________")
+let persons1 = firstJSONParserHandler.handle(data: data1)
+printPersonsOptional(persons1)
+print("____________________________________ DATA 2 ____________________________________")
+let persons2 = firstJSONParserHandler.handle(data: data2)
+printPersonsOptional(persons2)
+print("____________________________________ DATA 3 ____________________________________")
+let persons3 = firstJSONParserHandler.handle(data: data3)
+printPersonsOptional(persons3)
